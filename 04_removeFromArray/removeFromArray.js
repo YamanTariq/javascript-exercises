ff2e80c3db8e65f0df9085e025719ab780d689c6
YamanTariq <103046562+YamanTariq@users.noreply.github.com>
2024-12-17 22:51:09 +0500

    COMPLETE first 4 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    
+    if(arr == null) {
+        return null;
+    }
+    
+    let result = arr.filter( a => !targets.includes(a));
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    if(num2 < num1) {
+        var low = num2;
+        var high = num1;
+    } else {
+        var low = num1;
+        var high = num2;
+    }
+
+    let sum = 0;
+    for(let i = low; i <= high; i++){
+        sum+= i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
